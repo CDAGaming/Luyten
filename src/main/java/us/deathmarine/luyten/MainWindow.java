d0c4134b4913d959417a93812616e2bcca9e8ebd
CDAGaming <cstack2011@yahoo.com>
2020-05-05 20:48:11 -0500

    [Fix] Fixed Legal Button
    Also finalizes Refactor to src/main sourcesets

--- src/main/java/us/deathmarine/luyten/MainWindow.java
+++ src/main/java/us/deathmarine/luyten/MainWindow.java
@@ -272,14 +272,14 @@ public class MainWindow extends JFrame {
         StringBuilder sb = new StringBuilder();
         try {
             BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(getClass().getResourceAsStream("/distfiles/Procyon.License.txt")));
+                    new InputStreamReader(getClass().getResourceAsStream("/Procyon.License.txt")));
             String line;
             while ((line = reader.readLine()) != null) {
                 sb.append(line).append("\n");
             }
             sb.append("\n\n\n\n\n");
             reader = new BufferedReader(
-                    new InputStreamReader(getClass().getResourceAsStream("/distfiles/RSyntaxTextArea.License.txt")));
+                    new InputStreamReader(getClass().getResourceAsStream("/RSyntaxTextArea.License.txt")));
             while ((line = reader.readLine()) != null) {
                 sb.append(line).append("\n");
             }
